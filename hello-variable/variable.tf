91493e7a8fe3e055d5cc5e9abf0836b63fe48925
mayaworld13 <mayank07082001@gmail.com>
2024-05-18 23:00:23 +0530

    variable initiated

---
+++ hello-variable/variable.tf
@@ -0,0 +1,27 @@
+variable user{}
+
+
+## next possibility
+
+variable employee_name{}
+
+output print_employee {
+  value = var.employee_name
+}
+
+
+## next possibility
+variable student{}
+
+output print_student {
+  value = "var.student"
+}
+
+## next possibility
+variable username{}
+
+output printname {
+  value = "${var.username}"
+}
+
+
